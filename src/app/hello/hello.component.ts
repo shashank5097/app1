8c6728ed1c427cd4954da79512cb5170974e5fc7
Shashank Kannoji <shashank_kannoji@persistent.com>
2019-02-26 13:55:06 +0530

    2

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-hello',
-    templateUrl:'./hello.component.html'
+    templateUrl:'./hello.component.html',
+    styleUrls:['./hello.component.css']
 })
 export class HelloComponent{
     msg :string = "welcome to angular class";
     loc: string = "PSL,Nagpur";
-    schedule = '25Feb - 1 Mar'
+    schedule = '25Feb - 1 Mar';
+    count=0;
+    isDisabled:boolean=false;
+    display:boolean=false;
+    isSpecial:boolean=true;
+    canSave:boolean=true;
+    stylename:string ='s1'
 }
